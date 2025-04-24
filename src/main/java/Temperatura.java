a09db1d818308874936e586318a6b8635d6e1060
2025105070069 <2025105070069@aqs-lab-c-16.mshome.net>
2025-04-24 14:43:10 -0400

    Criada a classe TEmperatura e iniciada a implementação da classe TemperaturaEspecial

--- src/main/java/Temperatura.java
+++ src/main/java/Temperatura.java
@@ -1,3 +1,29 @@
 public class Temperatura {
+  //Atributos privados para aemazenar Temp - Celcius
+    private double tempCelcius;
     
-}
+    //construtor - criar o objeto com uma Temp inicial
+    public Temperatura(double tempInicialCelcius){
+        this.tempCelcius = tempInicialCelcius;
+     }
+        
+        //getter - para ler a temperatura
+    public double getTemperaturaCelcius(){
+    return tempCelcius;
+    }
+    
+    //setter - para mudar a temperatura
+    public void setTemperaturaCelcius(double mudarTempCelcius){
+    this.tempCelcius = mudarTempCelcius;
+    }
+    
+    //método para converte celcius para fahrenheit
+    public double converteParaFahrenheit(){
+    return (tempCelcius*9/5)+32;
+    }
+    
+    //método para converter Celcius para Kelvin
+    public double converterParaKelvin(){
+    return tempCelcius+273.15;
+    }
+}                                                                                                                                                                                                                                     
--- src/main/java/com/mycompany/conversotemperatura/ConversoTemperatura.java
+++ src/main/java/com/mycompany/conversotemperatura/ConversoTemperatura.java
@@ -1,6 +1,9 @@
 public class ConversoTemperatura {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+     Temperatura c1 = new Temperatura (50.5);
+     
+       // System.out.println("Temp em F: "+c1.converteParaFahrenheit());
+       
     }
 }
